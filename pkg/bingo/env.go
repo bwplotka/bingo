410828ca1f2907718420b0d1f715af156baef8d3
Kemal Akkoyun <kakkoyun@users.noreply.github.com>
2020-11-03 23:48:07 +0100

    Fix local function issue for variables.env (#47)
    * Fix local issue for variables.env
    
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>
    
    * Address review issues
    
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- pkg/bingo/env.go
+++ pkg/bingo/env.go
@@ -9,14 +9,14 @@ const (
 	envBinVarsTmpl = `# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo {{ .Version }}. DO NOT EDIT.
 # All tools are designed to be build inside $GOBIN.
 # Those variables will work only until 'bingo get' was invoked, or if tools were installed via Makefile's Variables.mk.
-local gobin=$(go env GOBIN)
+GOBIN=${GOBIN:=$(go env GOBIN)}
 
-if [ -z "$gobin" ]; then
-	gobin="$(go env GOPATH)/bin"
+if [ -z "$GOBIN" ]; then
+	GOBIN="$(go env GOPATH)/bin"
 fi
 
 {{range $p := .MainPackages }}
-{{ $p.EnvVarName }}="{{- range $i, $v := $p.Versions }}{{- if ne $i 0}} {{- end }}${gobin}/{{ $p.Name }}-{{ $v.Version }}{{- end }}"
+{{ $p.EnvVarName }}="{{- range $i, $v := $p.Versions }}{{- if ne $i 0}} {{- end }}${GOBIN}/{{ $p.Name }}-{{ $v.Version }}{{- end }}"
 {{ end}}
 `
 )
